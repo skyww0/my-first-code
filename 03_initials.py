53b946231bc320e90fa8ecc5e58888e98b422de1
skyww0 <Skyesomaya05@gmail.com>
2025-12-15 07:48:11 +0400

    Create 03_initials.py

---
+++ 03_initials.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+print("SSSS           SSSS")
+print("S              S")
+print("S              S")
+print("SSSS           SSSS")
+print("   S              S")
+print("   S              S")
+print("SSSS           SSSS")
